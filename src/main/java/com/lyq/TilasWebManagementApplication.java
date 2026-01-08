4ae7ae36ee079a1e2982552b8af0fa627f69ab6d
程翊雪 <1355784761@qq.com>
2026-01-08 23:16:10 +0800

    完成登录认证，配置了filter

--- src/main/java/com/lyq/TilasWebManagementApplication.java
+++ src/main/java/com/lyq/TilasWebManagementApplication.java
@@ -3,10 +3,12 @@ package com.lyq;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
 
 @MapperScan("com.lyq.mapper")   // 核心注解：指定Mapper接口所在的包路径
 @SpringBootApplication
+@ServletComponentScan //开启对Servlet组件的支持，不开启，过滤器无法生效
 public class TilasWebManagementApplication {
 
     public static void main(String[] args) {
